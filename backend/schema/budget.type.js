0d222bc8516689d4addb14433aa6e08bfddf0fdf
Tharani Malikha <53375286+tharani694@users.noreply.github.com>
2026-02-21 23:04:46 +0530

    Adding Mongo

--- backend/schema/budget.type.js
+++ backend/schema/budget.type.js
@@ -1,32 +1,42 @@
 const { gql } = require('apollo-server-express')
 
 module.exports = gql`
-    type BudgetCategory {
-        id: ID!
-        name: String!
-        allocated: Int
-        spent: Int
-    }
-    
-    type Budget {
-        total: Int
-        categories: [BudgetCategory]
-    }
+type Budget {
+  id: ID!
+  subEventId: ID!
+  categories: [BudgetCategory]
+}
 
-    extend type Query {
-        budget: Budget
-    }
-    
-    input UpdateBudgetCategory {
-        id: ID!
-        name: String
-        allocated: Int
-    }
+type BudgetCategory {
+  id: ID!
+  budgetId: ID!
+  name: String!
+  allocated: Int
+  spent: Int
+}
 
-    extend type Mutation {
-        updateBudgetTotal(total: Int!): Budget
-        addBudgetCategory(name: String!, allocated: Int): BudgetCategory
-        updateBudgetCategory(input: UpdateBudgetCategory!): BudgetCategory
-        deleteBudgetCategory(id: ID!): BudgetCategory
-    }
+extend type Query {
+  budget(subEventId: ID!): Budget
+  budgetCategories(budgetId: ID!): [BudgetCategory]
+}
+
+input UpdateBudgetCategoryInput {
+  id: ID!
+  name: String
+  allocated: Int
+}
+
+extend type Mutation {
+  addBudgetCategory(
+    budgetId: ID!
+    name: String!
+    allocated: Int
+  ): BudgetCategory
+
+  updateBudgetCategory(
+    input: UpdateBudgetCategoryInput!
+  ): BudgetCategory
+
+  deleteBudgetCategory(id: ID!): BudgetCategory
+}
 `
