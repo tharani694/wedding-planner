3f52b48e44446a6727bda1a39722255fb32d5678
Tharani Malikha <53375286+tharani694@users.noreply.github.com>
2026-02-21 17:07:21 +0530

    Update VendorPage.jsx

--- src/components/Vendors/VendorPage.jsx
+++ src/components/Vendors/VendorPage.jsx
@@ -2,26 +2,27 @@ import { useQuery } from "@apollo/client";
 import VendorForm from "./VendorForm";
 import VendorList from "./VendorList";
 import { GET_BUDGET, GET_VENDORS } from "../../graphql/queries";
-
-function VendorPage() {
-    const { data: budgetData, loading: budgetLoading, error: budgetError } = useQuery(GET_BUDGET)
+import '../style.css'
+const VendorPage = () => {
+    const { data: budgetData } = useQuery(GET_BUDGET)
     const { data, loading, error } = useQuery(GET_VENDORS);
+
+
     if (loading) return <p>Loading vendors...</p>;
     if (error) return <p>Error loading vendors</p>;
-    const budget = budgetData?.budget;
-    const categories = budget?.categories || [];
+
+
+    const categories = budgetData?.budget?.categories || [];
     const vendors = data?.vendors || [];
+
     return (
-        <>
+      <div className="vendor-page">
         <h2>Vendors</h2>
-        <VendorForm
-            categories={categories}
-        />
-        <VendorList
-            vendors={vendors}
-            categories={categories}
-        />
-        </>
+        <div className="vendor-grid">
+          <VendorForm categories={categories} />
+          <VendorList vendors={vendors} categories={categories} />
+        </div>
+      </div>
     );
     }
 
