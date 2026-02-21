c3c4a005f8f53a92cf7dd77f5f0c9595f6333512
Tharani Malikha <53375286+tharani694@users.noreply.github.com>
2026-02-21 17:02:39 +0530

    Update CategoryForm.jsx

--- src/components/Budget/CategoryForm.jsx
+++ src/components/Budget/CategoryForm.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { ADD_BUDGET_CATEGORY } from "../../graphql/mutations";
 import { GET_BUDGET } from "../../graphql/queries";
+import { TextField, Button, Stack, Grid } from "@mui/material";
 
-function CategoryForm() {
+const CategoryForm = () => {
   const [name, setName] = useState("");
   const [allocated, setAllocated] = useState("");
 
-  const [addBudgetCategory, { loading }] = useMutation(ADD_BUDGET_CATEGORY, {
-    refetchQueries: [{ query: GET_BUDGET , fetchPolicy: 'network-only'}],
-  });
+  const [addBudgetCategory, { loading }] = useMutation(
+    ADD_BUDGET_CATEGORY, 
+    { refetchQueries: [{ query: GET_BUDGET , fetchPolicy: 'network-only'}] }
+  );
 
   const handleAdd = async () => {
     if (!name.trim()) return;
@@ -30,23 +32,38 @@ function CategoryForm() {
   };
 
   return (
-    <div>
-      <input
-        placeholder="Category"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
+    <Grid container spacing={2} alignItems="center">
+      <Grid item xs={5}>
+        <TextField
+          label="Category"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          size="small"
+        />
+      </Grid>
 
-      <input
-        placeholder="Allocated"
-        value={allocated}
-        onChange={(e) => setAllocated(e.target.value)}
-      />
+      <Grid item xs={5}>
+        <TextField
+          label="Allocated"
+          type="number"
+          value={allocated}
+          onChange={(e) => setAllocated(e.target.value)}
+          size="small"
+        />
+      </Grid>
 
-      <button onClick={handleAdd} disabled={loading}>
-        {loading ? "Adding..." : "Add"}
-      </button>
-    </div>
+      <Grid item xs={2}>
+        <Button
+          variant="contained"
+          size="small"
+          onClick={handleAdd}
+          disabled={loading}
+          fullWidth
+          >
+            Add
+          </Button>
+        </Grid>
+      </Grid>
   );
 }
 
